890f837be6bdc6fb2f8f00db576a52e4cfcf0056
vinusingh36 <vineet.vinusingh36@gmail.com>
2024-04-01 11:10:56 +0530

    Basic Routing App

--- src/Components/Context/Login.jsx
+++ src/Components/Context/Login.jsx
@@ -46,8 +46,6 @@ const Login = ({ login }) => {
                 <input type="submit" name="" id="" value="Login" />
 
             </form>
-
-            <h2>Adding some text to check Deployment</h2>
         </div>
     )
 }
